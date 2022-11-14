d630fa760991b05d45346b28de32b9b01d349cc8
Panagiotis Hadjidoukas <phadjido@gmail.com>
2022-11-14 15:26:03 +0200

    case 6

--- mixing/6.standalone_c_mex_m/buildme.sh
+++ mixing/6.standalone_c_mex_m/buildme.sh
@@ -1,3 +1,5 @@
+set -e
+set -x
+cp ../4.mex_demo/ComputePrimes.mexa64 .
 mcc -W lib:libprime -T link:lib callprimes.m
-mbuild primedriver.c libprime.so
-# ComputePrimes.mexa64
+mbuild primedriver.c libprime.so # ComputePrimes.mexa64
