4e726c3bd292c52b7bfbb122105533f6f3477a08
Panagiotis Hadjidoukas <phadjido@gmail.com>
2022-11-15 14:42:49 +0200

    updated pptx

--- mixing/4.mex_demo/ComputePrimes.cpp
+++ mixing/4.mex_demo/ComputePrimes.cpp
@@ -8,21 +8,21 @@ int IsPrime(int n);
 
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
 {
-	if (nrhs != 1) 
+	if (nrhs != 1)
 	{
 		mexErrMsgTxt("One input required.");
-	} 
-	else if (nlhs > 1) 
+	}
+	else if (nlhs > 1)
 	{
 		mexErrMsgTxt("Too many output arguments");
-	}	
+	}
 
 	/* The input must be a noncomplex scalar integer.*/
 	int mrows, ncols;
 	mrows = mxGetM(prhs[0]);
 	ncols = mxGetN(prhs[0]);
-	
-	if (!mxIsDouble(prhs[0]) || mxIsComplex(prhs[0]) || 
+
+	if (!mxIsDouble(prhs[0]) || mxIsComplex(prhs[0]) ||
 		!(mrows == 1 && ncols == 1)) 
 	{
 		mexErrMsgTxt("Input must be a noncomplex scalar integer.");
@@ -30,26 +30,25 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
 
 	double x, *y;
 	/*
-		e.g.   
+		e.g.
 				*x=4,
 				*y=2, 3, 5, 7
 	*/
-	
+
 	x = mxGetScalar(prhs[0]);
-		
+
 	/* Create matrix for the return argument. */
 	plhs[0] = mxCreateDoubleMatrix(mrows /* 1 */, (int) x, mxREAL);
 
 	y = mxGetPr(plhs[0]);
-	
+
 	//call ComputePrimes subroutines to fill vector of primes
-	ComputePrimes(y, (int) x);	
+	ComputePrimes(y, (int) x);
 }
 
 void ComputePrimes(double* y, int n)
 {
 	int index=0, i=2;
-
 
 	while (index!=n)
 	{
